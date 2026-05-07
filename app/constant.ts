a388c7ec2b9a8ad1d2ef4855208f1dcf174a1216
M.Asif <capripio@gmail.com>
2026-05-07 13:43:04 +0500

    Change base url to api

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "LMS";
 export const APP_NAME_SLUG = "lms";
 export const APP_VERSION = "1.0.0";
-export const BASE_URL = "https://wordpress.local/wp-json";
+export const BASE_URL = "https://api.luuxtheorie.nl/wp-json";
 export const AUTH_KEY = "gxtyEeY14uvHF9F$UW";
